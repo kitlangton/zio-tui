5452c9724b32e26dd237d4fe429711142b036793
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-02-20 21:17:26 +0530

    make the update function async

--- modules/core/src/main/scala/tui/components/LineInput.scala
+++ modules/core/src/main/scala/tui/components/LineInput.scala
@@ -4,20 +4,20 @@ import tui.{TUI, TerminalApp, TerminalEvent}
 import tui.TerminalApp.Step
 import tui.view.View.string2View
 import tui.view.{KeyEvent, View}
-import zio.RIO
+import zio.*
 
 case class LineInput(prompt: String) extends TerminalApp[Any, String, String] {
 
   override def render(state: String): View =
     View.horizontal(0)(s"$prompt".bold.green, state.bold, View.text(" ").reversed.blinking)
 
-  override def update(state: String, event: TerminalEvent[Any]): Step[String, String] =
+  override def update(state: String, event: TerminalEvent[Any]): Task[Step[String, String]] =
     event match {
-      case TerminalEvent.SystemEvent(KeyEvent.Character(c)) => Step.update(state + c)
-      case TerminalEvent.SystemEvent(KeyEvent.Delete)       => Step.update(state.dropRight(1))
-      case TerminalEvent.SystemEvent(KeyEvent.Exit)         => Step.exit
-      case TerminalEvent.SystemEvent(KeyEvent.Enter)        => Step.succeed(state)
-      case _                                                => Step.update(state)
+      case TerminalEvent.SystemEvent(KeyEvent.Character(c)) => ZIO.succeed(Step.update(state + c))
+      case TerminalEvent.SystemEvent(KeyEvent.Delete)       => ZIO.succeed(Step.update(state.dropRight(1)))
+      case TerminalEvent.SystemEvent(KeyEvent.Exit)         => ZIO.succeed(Step.exit)
+      case TerminalEvent.SystemEvent(KeyEvent.Enter)        => ZIO.succeed(Step.succeed(state))
+      case _                                                => ZIO.succeed(Step.update(state))
     }
 }
 
